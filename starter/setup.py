2c4659b45cbc70a191a8ac459f38bab74ac95e66
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-02-17 21:14:12 +0400

    Update setup.py

--- starter/setup.py
+++ starter/setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
-    version="0.0.0",
+    name="Cemsus Prediction MLOps",
+    version="1.0.0",
     description="Starter code.",
-    author="Student",
+    author="Sidahmed Faisal",
 )

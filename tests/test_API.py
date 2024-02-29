e2ec7150ea34b62ab620f13b782248a41412caff
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-02-29 14:48:02 +0400

    added API endpoints testing

---
+++ tests/test_API.py
@@ -0,0 +1,48 @@
+import os, json , logging
+from fastapi.testclient import TestClient
+
+from app import app
+
+# Initialize logging
+logging.basicConfig(filename='training.log',
+                    level=logging.INFO,
+                    filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+# Create Client to test the API
+client = TestClient(app)
+
+# Test index get endpoint
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "App is running!"
+
+# Test prediction endpoint
+def test_predict():
+
+    data = {"age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 2174,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States"
+        }
+    response = client.post("/predict", data=json.dumps(data))
+    assert response.status_code == 200
+    assert response.json()['prediction'] == "this person earns  ('<=50K',)"
+
+# Test invalid request body
+def test_predict_invalid_request():
+    data = {}
+    response = client.post("/predict", json=json.dumps(data))
+    assert response.status_code == 422
+    logging.warning(f"The Request body has {len(data)} features you must provide 14 features")
